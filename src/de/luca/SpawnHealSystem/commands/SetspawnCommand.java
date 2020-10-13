a5b3995337fa87dd8c3ddab1fd5ee75ec4aa970b
LucaM2006 <72764739+LucaM2006@users.noreply.github.com>
2020-10-13 19:48:42 +0200

    Update SetspawnCommand.java

--- src/de/luca/SpawnHealSystem/commands/SetspawnCommand.java
+++ src/de/luca/SpawnHealSystem/commands/SetspawnCommand.java
@@ -1,42 +1,52 @@
+// This class was created by LucaHD
+
+
 package de.luca.SpawnHealSystem.commands;
 
+import de.luca.SpawnHealSystem.main.Main;
 import de.luca.SpawnHealSystem.util.ConfigUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import de.luca.SpawnHealSystem.main.Main;
+public class SetspawnCommand implements CommandExecutor {
 
-public class SetspawnCommand implements CommandExecutor{
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(!(sender instanceof Player)) {
-			sender.sendMessage(Main.getPrefix()+ "§cDu bist kein Spieler!");
-			return true;
-		}
-		
-		
-		final Player player = (Player) sender;
-		
-		if(!player.hasPermission("Setspawn.setspawn")) {
-			player.sendMessage(Main.getPrefix() + "§cDazu hast du keine Rechte!");
-			return true;
-			
-		}
-		if (args.length == 0){
-			ConfigUtils.config.set("Spawn",player.getLocation());
-			ConfigUtils.savedata();
-			player.sendMessage(Main.getPrefix() + "§aDu hast den §6Spawn §aerfolgreich gesetzt.");
-		}else {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-			player.sendMessage(Main.getPrefix()+ "§cBitte benutze §6/setspawn");
-		}
-		
-		
-		
-		return true;
-	}
+        if (!(sender instanceof Player)){
+            sender.sendMessage(Main.getPrefix()+ "§cDu musst ein Spieler sein.");
+            return true;
+        }
 
+        final Player player = ((Player) sender);
+
+        if (player.hasPermission("setspawn.setspawn")){
+            switch (args.length){
+                case 0:
+                    ConfigUtils.config.set("Spawn",player.getLocation());
+                    ConfigUtils.savedata();
+                    player.sendMessage(Main.getPrefix() + "§aDer §6Spawn §awurde erfolgreich gesetzt.");
+                    break;
+
+
+
+                default:
+                    player.sendMessage("§cBitte Benutze: /setspawn");
+                    break;
+            }
+
+
+
+
+        }else {
+            player.sendMessage(Main.getPrefix() + "§cDazu hast du keine Rechte!");
+        }
+
+
+
+        return true;
+    }
 }
