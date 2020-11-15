3e8249210c0c4668a8bc565e998a4e956f55c92d
LucaHDYT <72764739+LucaHDYT@users.noreply.github.com>
2020-11-15 21:21:36 +0100

    Update HealCommand.java

--- src/de/luca/SpawnHealSystem/commands/HealCommand.java
+++ src/de/luca/SpawnHealSystem/commands/HealCommand.java
@@ -1,4 +1,4 @@
-import de.luca.SpawnHealSystem.main.Main;
+import de.lucahdyt.SpawnHealSystem.main.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
