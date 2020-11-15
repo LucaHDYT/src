5d6f83f19025336bdbae87b51a8467bc57e96f3e
LucaHDYT <72764739+LucaHDYT@users.noreply.github.com>
2020-11-15 21:24:31 +0100

    Update ScoreboardHandler.java

--- src/de/luca/SpawnHealSystem/scoreboard/ScoreboardHandler.java
+++ src/de/luca/SpawnHealSystem/scoreboard/ScoreboardHandler.java
@@ -1,4 +1,4 @@
-import de.luca.SpawnHealSystem.main.Main;
+import de.lucahdyt.SpawnHealSystem.main.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.DisplaySlot;
